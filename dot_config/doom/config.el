f159569c64588a520fd83b7a6b3c6bb2ca7b8c1f
Pietro F. Maggi <pfm@pietromaggi.com>
2022-08-21 23:39:29 +0200

    Update dot_config/doom/config.el

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -21,7 +21,7 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "Source Code Pro for Powerline" :size 14))
+;; (setq doom-font (font-spec :family "Source Code Pro for Powerline" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -36,6 +36,8 @@
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
+
+(setq projectile-project-search-path '("~/Soources/"))
 
 (after! geiser
   (setq geiser-chez-binary (executable-find "chez"))
