4716283c6331c3304fb0e95d1427414441740b03
Pietro F. Maggi <pfm@pietromaggi.com>
2022-08-21 23:14:06 +0200

    Update dot_config/doom/init.el

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -110,7 +110,7 @@
 
        :lang
        ;;agda              ; types of types of types of types...
-       cc                  ; C/C++/Obj-C madness
+       (cc +lsp)                  ; C/C++/Obj-C madness
        ;;clojure             ; java with a lisp
        common-lisp         ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -159,7 +159,7 @@
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       rust                ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp)         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        (scheme +chez)      ; a fully conniving family of lisps
        sh                  ; she sells {ba,z,fi}sh shells on the C xor
@@ -169,6 +169,7 @@
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
        ;;yaml              ; JSON, but readable
+       (zig +lsp)
 
        :email
        ;;(mu4e +gmail)
