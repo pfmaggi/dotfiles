5632091826b70067f943737c8abbea7c9c61b752
Pietro F. Maggi <pfm@pietromaggi.com>
2022-08-23 21:37:48 +0200

    Update dot_config/doom/init.el

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -152,7 +152,7 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python +lsp
-               +conda)     ; beautiful is better than ugly
+               +poetry)     ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
